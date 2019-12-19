d669ae8d0bfbe50a8277f9e4550d13d79c797fec
anant007 <anant.awasthy29@gmail.com>
2019-12-19 23:08:25 +0530

    done

--- src/store.js
+++ src/store.js
@@ -22,15 +22,15 @@ const middelware = [thunk]
 //  }
 
 
-//const store = createStore(rootReducer, initialState , applyMiddleware(...middelware))
+const store = createStore(rootReducer, initialState , applyMiddleware(...middelware))
 
 
-const store = createStore(rootReducer, initialState ,
-compose(
-    applyMiddleware(...middelware), 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-)
+// const store = createStore(rootReducer, initialState ,
+// compose(
+//     applyMiddleware(...middelware), 
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+// )
 
 
 export default store
